013ac7392fb22c0c4cdeedd7ae146caa53598152
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2022-10-22 11:57:55 +0900

    Add files via upload

--- lotto.py
+++ lotto.py
@@ -1,16 +1,23 @@
 ﻿import random
 lotto = []
-i = 1
-while i <= 45:
-    lotto.append(i)
-    i+=1
-# lotto = [1~45]
-lottosize = len(lotto) # 45
-j = 1
-while j <= 6:
-    lottosize -= 1
-    idx = random.randint(0, lottosize) 
-    # 0 ~ 44, 0 ~ 43, 0 ~ 42, 0 ~ 41, 0 ~ 40
-    lottoNum = lotto.pop(idx)
-    print(lottoNum, end =", ")
-    j += 1
+qty = int(input("원하는 수를 입력하세요. : "))
+cnt = 1
+while cnt <= qty:
+    i = 1
+    while i <= 45:
+        lotto.append(i) 
+        i+=1
+    # print(lotto) # 1~45
+    
+    lottosize = len(lotto) # 45
+    j = 1
+    while j <= 6:
+        lottosize -= 1
+        idx = random.randint(0, lottosize) 
+        # 0 ~ 44, 0 ~ 43, 0 ~ 42, 0 ~ 41, 0 ~ 40
+        lottoNum = lotto.pop(idx)
+        print(lottoNum, end =", ")
+        j += 1
+    cnt += 1
+    lotto.clear()
+    print()
