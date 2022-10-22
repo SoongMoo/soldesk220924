4228614a377115740fe402a080cbfb6f2ddeafd0
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2022-10-22 12:03:41 +0900

    Add files via upload

---
+++ mod.py
@@ -0,0 +1,26 @@
+﻿PI = 3.141592
+
+def sum1(num1, num2):
+    result = num1 + num2
+    return result
+
+class FourCal1:
+    def __init__(self, num1, num2): 
+        self.first = num1
+        self.second = num2
+    def add(self):
+        self.result = self.first + self.second
+    def div(self):
+        self.result = self.first / self.second
+
+class MoreFourCal1(FourCal1): # add(), div()
+    def mul(self):
+        self.result = self.first * self.second
+    def sub(self):
+        self.result = self.first - self.second
+
+    def div(self): 
+        if self.second == 0:
+            self.result = 0
+        else :
+            self.result = self.first / self.second
